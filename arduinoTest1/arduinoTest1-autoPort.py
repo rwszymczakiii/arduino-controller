f40f318a79e011142ed1be0fff5be19df481bdca
Robert Szymczak <robbie@Roberts-MacBook-Pro.local>
2019-11-13 10:55:11 -0500

    write to serial

--- arduinoTest1/arduinoTest1-autoPort.py
+++ arduinoTest1/arduinoTest1-autoPort.py
@@ -13,15 +13,9 @@ class App:
     def __init__(self, master):
         frame = tk.Frame(master)
         frame.pack()
-        
-        # COM PORT 
-        tk.Label(frame, text='COM PORT: ').grid(row=0, column=0)
-        self.com_var = tk.StringVar()
-        tk.Entry(frame, textvariable=self.com_var).grid(row=0, column=1)
-        self.com_button = tk.Button(frame, text='SET COM PORT', command=self.set_com)
-        self.com_button.grid(row=1, columnspan=2)
-        self.com_response = tk.StringVar()
-        tk.Label(frame, textvariable=self.com_response).grid(row=2, columnspan=2)
+
+        self.arduinoData = serial.Serial('/dev/cu.usbmodem142101', 9600, timeout=0.5)
+        sleep(0.5)
         
         # LED ON/OFF
         self.on_button = tk.Button(frame, text='ON ', command=self.led_on)
@@ -33,16 +27,9 @@ class App:
         self.status_report = tk.StringVar()
         tk.Label(frame, textvariable=self.status_report).grid(row=4, columnspan=2)
 
-    def set_com(self):
-        self.com_response.set('connecting...')
-        self.com_port = self.com_var.get()
-        self.arduinoData = serial.Serial(f'{self.com_port}', 9600)
-        sleep(0.500)
-        self.com_response.set('COM PORT has been set')
-
     def led_on(self):
         self.arduinoData.write(b'1')
-        self.status_report.set(self.arduinoData.read(2))    
+        self.status_report.set(self.arduinoData.read(1))    
     def led_off(self):
         self.arduinoData.write(b'0')
         self.status_report.set('off')  
