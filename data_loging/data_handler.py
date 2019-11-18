efa0e3dfcb3e00a90aa54df5c80b14333cffefff
Robert Szymczak <robbie@Roberts-MacBook-Pro.local>
2019-11-18 16:57:14 -0500

    dataloging

---
+++ data_loging/data_handler.py
@@ -0,0 +1,56 @@
+import csv
+from datetime import datetime
+
+sample_data = ['','a','','','','b', '','','c','','','','d', '','','e','a','','','','b', '','','c','','','','d', '','','e']
+
+def convert_data(data):
+  converted_data = []
+  total_puff_count = 0
+  cycle_count = 0
+  a = f'Pump # : 1    at {datetime.now()}'
+  b = f'Pump # : 2    at {datetime.now()}'
+  c = f'Pump # : 3    at {datetime.now()}'
+  d = f'Pump # : 4    at {datetime.now()}'
+  e = f'Pump # : 5    at {datetime.now()}'
+
+  while ('' in data):
+    data.remove('')
+
+  for i in data: 
+    if i == 'a':
+      cycle_count += 1
+      a2 = f'--- Cycle # : {cycle_count}    at {datetime.now()} --- '
+      converted_data.append(a2)
+      converted_data.append(a)
+      total_puff_count += 1
+    elif i == 'b':
+      converted_data.append(b)
+      total_puff_count += 1
+    elif i == 'c':
+      converted_data.append(c)
+      total_puff_count += 1
+    elif i == 'd':
+      converted_data.append(d)
+      total_puff_count += 1
+    elif i == 'e':
+      converted_data.append(e)
+      total_puff_count += 1
+  
+  converted_data.append(f'Run finished at {datetime.now()}')
+  converted_data.append(f'Total Puffs : {total_puff_count}')
+
+  return converted_data
+
+converted_data = convert_data(sample_data)
+print(converted_data)
+
+def data_to_csv(data):
+  with open("data_report.csv",'w') as f:
+    writer = csv.writer(f,delimiter=" ")
+    for i in data: 
+      writer.writerow(i) 
+
+data_to_csv(converted_data)
+
+
+
---
+++ data_loging/data_log_test1.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -\*-
+"""
+Created on Fri Nov  1 14:01:19 2019
+
+@author: rwszy
+"""
+
+import serial
+import tkinter as tk
+import time
+import csv
+import threading
+
+class App:
+    def __init__(self, master):
+        frame = tk.Frame(master)
+        frame.pack()
+
+        self.arduinoData = serial.Serial('/dev/cu.usbmodem142101', 9600, timeout=1)
+        time.sleep(0.5)
+        
+        # LED ON/OFF
+        self.on_button = tk.Button(frame, text='ON ', command=self.led_on)
+        self.on_button.grid(row=3, column=0)
+        self.off_button = tk.Button(frame, text='OFF', command=self.led_off)
+        self.off_button.grid(row=3, column=1)
+
+        # LOG
+        self.status_report = tk.StringVar()
+        tk.Label(frame, textvariable=self.status_report).grid(row=4, columnspan=2)     
+
+    def led_on(self):
+        self.status_report.set('on')
+        time.sleep(0.2)
+        self.arduinoData.write(b'1')
+        def print_serial():
+            data_list = []
+            while 1:
+                try:
+                    data = self.arduinoData.read().decode('utf-8')
+                    data_list.append(data)
+                    print(data_list)
+                except:
+                    with open("dLT1_data.csv",'w') as f:
+                        writer = csv.writer(f,delimiter=",")
+                        writer.writerow(data) 
+        print_serial = threading.Thread(target=print_serial)
+        print_serial.start()
+
+    def led_off(self):
+        self.status_report.set('off')
+        self.arduinoData.close()
+        self.arduinoData.open()
+ 
+root = tk.Tk()
+root.geometry('300x150')
+root.wm_title('Arduino Controller')  
+app = App(root)
+root.mainloop()
