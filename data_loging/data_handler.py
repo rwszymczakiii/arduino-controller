89be7131de59804f2b58b78a49d1186b490b2ccb
Robert Szymczak <robbie@Roberts-MacBook-Pro.local>
2019-11-22 08:53:05 -0500

    god bless stack overflow

--- data_loging/data_handler.py
+++ data_loging/data_handler.py
@@ -1,7 +1,8 @@
 import csv
 from datetime import datetime
+from itertools import zip_longest
 
-sample_data = ['','a','a','a','a','a','a','','b','b','b','b','b','b','b','b','','','c','c','c','c','c','c','','','','d','d','d','d','d','d','d','d','','','e','e','e','e','e','e','','','a','a','a','a','a','a','','','','b','b','b','b','b','b','b','b','b','','','c','c','c','c','c','c','','','','d','d','d','d','d','d','','','e','e','e','e','e','e','','']
+sample_data = ['','a','a','a','a','a','a','','b','b','b','b','b','b','b','b','','','c','c','c','c','c','c','','','','d','d','d','d','d','d','d','d','','','e','e','e','e','e','e','','','a','a','a','a','a','a','','','','b','b','b','b','b','b','b','b','b','','','c','c','c','c','c','c','','','','d','d','d','d','d','d','','','e','e','e','e','e','e','','','','a','a','a','a','a','a','','b','b','b','b','b','b','b','b','','','c','c','c','c','c','c','','','','d','d','d','d','d','d','d','d','','','e','e','e','e','e','e','','','a','a','a','a','a','a','','','','b','b','b','b','b','b','b','b','b','','','c','c','c','c','c','c','','','','d','d','d','d','d','d','','','e','e','e','e','e','e','','']
 
 def convert_data(data):
   converted_data = []
@@ -11,24 +12,15 @@ def convert_data(data):
   while ('' in data):
     data.remove('')
 
-  # for i in data: 
-  #   if i == data[data.index(i)+1]:
-  #     data.remove(i)
+  def remove_consecutive_duplicates(lst):
+    ahead = iter(lst)
+    next(ahead)
+    return [ x for x, y in zip_longest(lst, ahead) if x and x != y ]
 
-  # print(data)
+  data = remove_consecutive_duplicates(data)
 
-  # def remove_duplicates(n):
-  #   while (data[n] == data[n+1]):
-  #     data.remove(data[n])
-  #   return data
+  print(data)
 
-  # for i in data:
-  #   remove_duplicates(i)
-  
-  # for i in data: 
-  #   if i == data[data.index(i) + 1]:
-  #     remove_duplicates(i)
-  #   elif i != data
 
   a = f'Pump # : 1    at {datetime.now()}'
   b = f'Pump # : 2    at {datetime.now()}'
@@ -69,6 +61,7 @@ def convert_data(data):
   return converted_data
 
 converted_data = convert_data(sample_data)
+print(converted_data)
 
 def data_to_csv(data):
   with open("./data_loging/data_report.csv",'w') as f:
@@ -77,31 +70,3 @@ def data_to_csv(data):
       writer.writerow([i]) 
 
 data_to_csv(converted_data)
-
-
-# data = ['a', 'a', 'a', 'a', 'a', 'a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'c', 'c', 'c', 'c', 'c', 'c']
-# print(len(data))
-# print(data[data.index('b')])
-
-# d = ord('d')
-# c = ord('c')
-# a = ord('a')
-# print(a)
-
-# data = ['a', 'a', 'a', 'a', 'a', 'a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'c', 'c', 'c', 'c', 'c', 'c', 'a', 'a', 'a', 'a', 'a', 'a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'c', 'c', 'c', 'c', 'c', 'c']
-# ord_data = []
-# for i in data: 
-#   ord_data.append(ord(i))
-# print(ord_data)
-
-data = ['a', 'a', 'a', 'a', 'a', 'a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'c', 'c', 'c', 'c', 'c', 'c', 'a', 'a', 'a', 'a', 'a', 'a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'c', 'c', 'c', 'c', 'c', 'c']
-ord_data = []
-for i in data: 
-  ord_data.append(ord(i))
-counter = 0
-for i in ord_data:
-  if i > ord_data[ord_data.index(i)+1]:
-    counter += 1
-new_data = []
-# puting characters from data into a set (to removw duplicates) and appending that set into a new list n number of times where n is counter.value after the previoius for loop
-
